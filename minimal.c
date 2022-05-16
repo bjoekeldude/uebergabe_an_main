a6784dc4abd711ff078916913230fb781e233310
Steffo <bjoekeldude@gmail.com>
2022-05-16 21:38:42 +0200

    fixt -d option

--- minimal.c
+++ minimal.c
@@ -11,32 +11,35 @@ int main(int argc,char *argv[])
 	opterr = 0;
 	
 
-	while ((option = getopt(argc, argv, "hd:")) != -1)
+	while ((option = getopt(argc, argv, "hd:f:")) != -1)
 	switch (option)
       	{	
 		//minimal
+		case 'f':
+			printf("Hier das eingegebene Argument: %s\n", optarg);
+			break;
       		case 'h':
-        	printf("-h for help \n-d [text] um Text auszugeben\n");
-        	break;
-      		case 'v':
-		vvalue = optarg;
-		printf("%s\n", vvalue);
+        		printf("-h for help \n-d [text] um Text auszugeben\n");
+        		break;
+      		case 'd':
+			vvalue = optarg;
+			printf("%s\n", vvalue);
         	break;
 		//optional error handling
       		case '?':
-        	if (optopt == 'd')
-		{
-        		fprintf (stderr, "This is not 'Nam, this is programming. There are rules! -%c requires an argument.\n", optopt);
-		}       	
-		else if (isprint (optopt))//is character printable
-		{        
-			fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        	}
-		else
-		{
-        		fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
-		}
-        	return 1;
+        		if (optopt == 'd')
+			{
+        			fprintf (stderr, "This is not 'Nam, this is programming. There are rules! -%c requires an argument.\n", optopt);
+			}       	
+			else if (isprint (optopt))//is character printable
+			{        
+				fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        		}
+			else
+			{
+        			fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+			}
+        		return 1;
       		default:
         	abort ();
       }
